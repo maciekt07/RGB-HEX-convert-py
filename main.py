f55c53617f6a3569dd0318b37e0a9f57dc8e61ac
MaciekT07 <85953204+maciekkoks@users.noreply.github.com>
2021-12-18 18:15:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import random
 r = random.randint(0,255);
 g = random.randint(0,255);
 b = random.randint(0,255);
-solution = '%02x%02x%02x' % (r, g, b);
+solution = '#%02x%02x%02x' % (r, g, b);
 print("replace RGB to HEX game, author: github.com/maciekkoks");
 print("RGB:", r, g, b);
-input1 = input("Enter a hex code: #");
+input1 = input("Enter a hex code:");
 if (input1 == solution or input1 == solution.upper()): 
   print("the answer is correct!");
 else: 
-  print("the answer is not correct, the correct answer is:", solution);
+  print("the answer is not correct, the correct answer is:" ,solution);
 input("press enter to exit");
