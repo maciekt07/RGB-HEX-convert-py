d4c6d156adf2d58bfdb3915da5aed7053eaf3bb4
Maciej Twarog <85953204+maciekt07@users.noreply.github.com>
2022-07-10 23:58:59 +0200

    Update hex-rgb convert.py

--- hex-rgb convert.py
+++ hex-rgb convert.py
@@ -8,9 +8,9 @@ def choice1(): # rgb to hex
   wynik = '#%02x%02x%02x' % (r, g, b);
   print("rgb", r, g, b, "to hex format is:", wynik) 
 def choice2(): # hex to rgb
-    hex_kod = input("podaj kod hex: ")
-    hex_kod = hex_kod.replace("#", "")
-    print("hex",hex_kod, "to rgb format is:", tuple(int(hex_kod[i:i+2], 16) for i in (0, 2, 4)))
+    hex_code = input("enter hex code: ")
+    hex_code = hex_code.replace("#", "")
+    print("hex",hex_code, "to rgb format is:", tuple(int(hex_code[i:i+2], 16) for i in (0, 2, 4)))
 if (choice == "1"):
    choice1()
 if (choice == "2"):
